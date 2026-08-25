429d440856d572956527249912e25ad196b972b6
Ana Paula Matos Damasceno <anapaulamatosdamasceno@gmail.com>
2026-08-25 16:14:36 -0400

    fix:configura a rota de login como "

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Setores from '../pages/Setores/Setores';
 import Manager from '../pages/Manager/Manager';
-import Login from '../pages/Login/Login';
 import Relatorio from '../pages/Relatorio/Relatorio';
 import Motoristas from '../pages/Motorista/Motorista';
 import Login2 from '../pages/Login2/Login2'
@@ -11,13 +10,12 @@ const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Login2 />} />
+                <Route path="/Home" element={<Home />} />
                 <Route path="/setores" element={<Setores />} />
                 <Route path="/manager" element={<Manager />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/relatorio" element={<Relatorio />} />
                 <Route path="/motoristas" element={<Motoristas />} />
-                <Route path="/login2" element={<Login2 />} />
             </Routes>
         </BrowserRouter>
     )
