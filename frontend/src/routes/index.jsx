bc7ef057bfec4948aa48f72b868b858bab116b07
Ana Paula Matos Damasceno <anapaulamatosdamasceno@gmail.com>
2026-08-26 16:22:28 -0400

    fix: corrige nomeação da page login2 para: login

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,13 +4,13 @@ import Setores from '../pages/Setores/Setores';
 import Manager from '../pages/Manager/Manager';
 import Relatorio from '../pages/Relatorio/Relatorio';
 import Motoristas from '../pages/Motorista/Motorista';
-import Login2 from '../pages/Login2/Login2'
+import Login from '../pages/Login/Login'
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login2 />} />
+                <Route path="/" element={<Login />} />
                 <Route path="/chamado" element={<Home />} />
                 <Route path="/setores" element={<Setores />} />
                 <Route path="/manager" element={<Manager />} />
